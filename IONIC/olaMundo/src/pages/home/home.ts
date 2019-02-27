c3991e0494c99af80a28d82f937d5c8aff001893
PLACIDO ANTONIO LANGER REINERT <40044062+placidoalr@users.noreply.github.com>
2019-02-26 21:50:29 -0300

    TMNOCU

--- IONIC/olaMundo/src/pages/home/home.ts
+++ IONIC/olaMundo/src/pages/home/home.ts
@@ -1,3 +1,5 @@
+
+
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public listaAlunos = [];
+  public exibirConteudo : boolean = true;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  public limparAlunos(){
+    this.listaAlunos = [];
+  }
+
+  public adicionarAlunos(){
+    this.listaAlunos.push({nome : "Aluno A"});
+    this.listaAlunos.push({nome : "Aluno B"});
+    this.listaAlunos.push({nome : "Aluno C"});
+    this.listaAlunos.push({nome : "Aluno D"});
+  }
+buttonClick(){
+  this.exibirConteudo = !this.exibirConteudo;
+
+ }
 
 }
